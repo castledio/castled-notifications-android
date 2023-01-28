60b9ca57ed61b91dfedef889def177c7a427f18c
vinit-fdd9f <dheeraj.osw+vinit-fdd9f@gmail.com>
2023-01-28 21:05:28 +0530

    UI popup fullscreen

--- app/src/main/java/io/castled/notifications/FirstFragment.java
+++ app/src/main/java/io/castled/notifications/FirstFragment.java
@@ -22,7 +22,7 @@ public class FirstFragment extends Fragment {
 
     @Override
     public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
+            @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
 
@@ -37,11 +37,26 @@ public class FirstFragment extends Fragment {
         binding.buttonFirst.setOnClickListener(view1 -> NavHostFragment.findNavController(FirstFragment.this)
                 .navigate(R.id.action_FirstFragment_to_SecondFragment));
 
+        binding.btnLaunchPopup.setOnClickListener(btnLaunchPopupView -> TriggerPopup.Companion.showDialog(
+                requireContext(),
+                "#f8ffbd",
+                new PopupHeader("Summer sale is Back!", "#FFFFFF" , 18, "#E74C3C"),
+                new PopupMessage("30% offer on Electronics, Cloths, Sports and other categories.","#FFFFFF" , 12, "#039ADC"),
+//                    "http://i.imgur.com/DvpvklR.png",
+//                    "https://www.pakainfo.com/wp-content/uploads/2021/09/image-url-for-testing.jpg",
+                "https://www.infogrepper.com/wp-content/uploads/2022/10/image-url-for-testing.png",
+                "https://www.apple.com/",
+                new PopupPrimaryButton("Skip Now", "#000000", "#ffffff", "#000000", "https://www.google.com/"),
+                new PopupSecondaryButton("Start Shopping", "#ffe0da", "#FF6D07", "#5cdb5c","https://stackoverflow.com/")
+        ));
+
+        binding.btnLaunchFullscreenPopup.setOnClickListener(btnLaunchFullscreenPopupView -> {
+//            TriggerPopup.Companion.showFullScreenDialog(requireContext(), requireActivity());
 
 
-        binding.btnLaunchPopup.setOnClickListener(btnLaunchPopupView -> {
-            TriggerPopup.Companion.showDialog(
+            TriggerPopup.Companion.showDialog2(
                     requireContext(),
+                    requireActivity(),
                     "#f8ffbd",
                     new PopupHeader("Summer sale is Back!", "#FFFFFF" , 18, "#E74C3C"),
                     new PopupMessage("30% offer on Electronics, Cloths, Sports and other categories.","#FFFFFF" , 12, "#039ADC"),
@@ -52,6 +67,7 @@ public class FirstFragment extends Fragment {
                     new PopupPrimaryButton("Skip Now", "#000000", "#ffffff", "#000000", "https://www.google.com/"),
                     new PopupSecondaryButton("Start Shopping", "#ffe0da", "#FF6D07", "#5cdb5c","https://stackoverflow.com/")
             );
+
         });
     }
 
--- notifications/src/main/java/io/castled/inappNotifications/modal/TriggerPopup.kt
+++ notifications/src/main/java/io/castled/inappNotifications/modal/TriggerPopup.kt
@@ -1,21 +1,26 @@
 package io.castled.inappNotifications.modal
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
+import android.graphics.Rect
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.GradientDrawable
 import android.net.Uri
 import android.util.TypedValue
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.view.Window
+import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
 import io.castled.notifications.R
-import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 
@@ -39,7 +44,7 @@ class TriggerPopup {
             dialog.setCancelable(false)
             dialog.setContentView(R.layout.dialog_popup_triggered)
 
-            val frameLayout: FrameLayout = dialog.findViewById(R.id.root)
+            val frameLayout: FrameLayout = dialog.findViewById(R.id.frame_layout_root)
             val gradientDrawable: GradientDrawable = frameLayout.background as GradientDrawable
             gradientDrawable.setColor(Color.parseColor(returnDefaultOrValidHexColor(popUpBackgroundColor, "#FFFFFF")))
 
@@ -71,7 +76,7 @@ class TriggerPopup {
 
             val gradientDrawableButtonPrimary = GradientDrawable()
             gradientDrawableButtonPrimary.setColor(Color.parseColor(returnDefaultOrValidHexColor(popupPrimaryButton.buttonColor, "#FFFFFF")))
-            gradientDrawableButtonPrimary.cornerRadius = 10f;
+            gradientDrawableButtonPrimary.cornerRadius = 10f
             gradientDrawableButtonPrimary.setStroke(2, Color.parseColor(returnDefaultOrValidHexColor(popupPrimaryButton.borderColor, "#FFFFFF")))
 
             val btnPrimary: TextView = dialog.findViewById(R.id.btn_primary)
@@ -90,7 +95,7 @@ class TriggerPopup {
 
             val gradientDrawableButtonSecondary = GradientDrawable()
             gradientDrawableButtonSecondary.setColor(Color.parseColor(returnDefaultOrValidHexColor(popupSecondaryButton.buttonColor, "#FFFFFF")))
-            gradientDrawableButtonSecondary.cornerRadius = 10f;
+            gradientDrawableButtonSecondary.cornerRadius = 10f
             gradientDrawableButtonSecondary.setStroke(2, Color.parseColor(returnDefaultOrValidHexColor(popupSecondaryButton.borderColor, "#FFFFFF")))
 
             val btnSecondary: TextView = dialog.findViewById(R.id.btn_secondary)
@@ -113,10 +118,127 @@ class TriggerPopup {
         private fun returnDefaultOrValidHexColor(hexColor: String , defaultHexColor: String): String {
 //            val colorPattern: Pattern = Pattern.compile("#([0-9a-f]{3}|[0-9a-f]{6}|[0-9a-f]{8})")
             val colorPattern: Pattern = Pattern.compile("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$")
-            val m: Matcher = colorPattern.matcher(hexColor)
-            if (colorPattern.matcher(hexColor).matches()) return hexColor
-            else if (colorPattern.matcher(defaultHexColor).matches()) return defaultHexColor
+            return if (colorPattern.matcher(hexColor).matches()) hexColor
+            else if (colorPattern.matcher(defaultHexColor).matches()) defaultHexColor
             else throw Exception()
+        }
+
+        fun showFullScreenDialog(context: Context, activity: Activity){
+            val displayRectangle = Rect()
+            val window: Window = activity.window
+            window.decorView.getWindowVisibleDisplayFrame(displayRectangle)
+            val builder = AlertDialog.Builder(context, R.style.CustomAlertDialog)
+            val viewGroup: ViewGroup = activity.findViewById(android.R.id.content)
+            val dialogView: View =
+                LayoutInflater.from(context).inflate(R.layout.dialog_popup_fullscreen_triggered, viewGroup, false)
+            dialogView.minimumWidth = (displayRectangle.width() * 1f).toInt()
+            dialogView.minimumHeight = (displayRectangle.height() * 1f).toInt()
+            builder.setView(dialogView)
+            val alertDialog = builder.create()
+            val buttonOk = dialogView.findViewById<Button>(R.id.buttonOk)
+            buttonOk.setOnClickListener { alertDialog.dismiss() }
+            alertDialog.show()
+        }
+
+        fun showDialog2(
+            context: Context,
+            activity: Activity,
+            popUpBackgroundColor: String,
+            popUpHeader: PopupHeader,
+            popupMessage: PopupMessage,
+            imageUrl:String,
+            urlForOnClickOnImage: String,
+            popupPrimaryButton: PopupPrimaryButton,
+            popupSecondaryButton: PopupSecondaryButton
+        ) {
+            val displayRectangle = Rect()
+            val window: Window = activity.window
+            window.decorView.getWindowVisibleDisplayFrame(displayRectangle)
+
+            val builder = AlertDialog.Builder(context, R.style.CustomAlertDialog)
+            val viewGroup: ViewGroup = activity.findViewById(android.R.id.content)
+            val dialogView: View =
+                LayoutInflater.from(context).inflate(R.layout.dialog_popup_triggered, viewGroup, false)
+            dialogView.minimumWidth = (displayRectangle.width() * 1f).toInt()
+            dialogView.minimumHeight = (displayRectangle.height() * 1f).toInt()
+            builder.setView(dialogView)
+            val dialog = builder.create()
+
+//            val dialog = Dialog(context)
+//            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+//            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+//            dialog.setCancelable(false)
+//            dialog.setContentView(R.layout.dialog_popup_triggered)
+
+            val frameLayout: FrameLayout = dialogView.findViewById(R.id.frame_layout_root)
+            val gradientDrawable: GradientDrawable = frameLayout.background as GradientDrawable
+            gradientDrawable.setColor(Color.parseColor(returnDefaultOrValidHexColor(popUpBackgroundColor, "#FFFFFF")))
+
+            val view: View = dialogView.findViewById(R.id.view_close)
+            view.setOnClickListener { dialog.dismiss() }
+
+            val imageView: ImageView = dialogView.findViewById(R.id.img_popup)
+            Glide.with(context).load(imageUrl).into(imageView)
+            imageView.setOnClickListener {
+                if (urlForOnClickOnImage.isNotEmpty()){
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(urlForOnClickOnImage))
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    context.startActivity(intent)
+                }
+
+            }
+
+            val textHeader: TextView = dialogView.findViewById(R.id.txt_header)
+            textHeader.setBackgroundColor(Color.parseColor(returnDefaultOrValidHexColor(popUpHeader.backgroundColor, "#FFFFFF")))
+            textHeader.setTextColor(Color.parseColor(returnDefaultOrValidHexColor(popUpHeader.fontColor, "#000000")))
+            textHeader.setTextSize(TypedValue.COMPLEX_UNIT_SP,popUpHeader.fontSize)
+            textHeader.text = popUpHeader.header
+
+            val textMessage: TextView = dialogView.findViewById(R.id.txt_message)
+            textMessage.setBackgroundColor(Color.parseColor(returnDefaultOrValidHexColor(popupMessage.backgroundColor, "#FFFFFF")))
+            textMessage.setTextColor(Color.parseColor(returnDefaultOrValidHexColor(popupMessage.fontColor, "#000000")))
+            textMessage.setTextSize(TypedValue.COMPLEX_UNIT_SP,popupMessage.fontSize)
+            textMessage.text = popupMessage.message
+
+            val gradientDrawableButtonPrimary = GradientDrawable()
+            gradientDrawableButtonPrimary.setColor(Color.parseColor(returnDefaultOrValidHexColor(popupPrimaryButton.buttonColor, "#FFFFFF")))
+            gradientDrawableButtonPrimary.cornerRadius = 10f
+            gradientDrawableButtonPrimary.setStroke(2, Color.parseColor(returnDefaultOrValidHexColor(popupPrimaryButton.borderColor, "#FFFFFF")))
+
+            val btnPrimary: TextView = dialogView.findViewById(R.id.btn_primary)
+            btnPrimary.background = gradientDrawableButtonPrimary
+            btnPrimary.setTextColor(Color.parseColor(returnDefaultOrValidHexColor(popupPrimaryButton.fontColor, "#000000")))
+            btnPrimary.text = popupPrimaryButton.buttonText
+            btnPrimary.setOnClickListener {
+                dialog.dismiss()
+                if (popupPrimaryButton.urlOnClick.isNotEmpty()){
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(popupPrimaryButton.urlOnClick))
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//                if (intent.resolveActivity(context.packageManager) != null)
+                    context.startActivity(intent)
+                }
+            }
+
+            val gradientDrawableButtonSecondary = GradientDrawable()
+            gradientDrawableButtonSecondary.setColor(Color.parseColor(returnDefaultOrValidHexColor(popupSecondaryButton.buttonColor, "#FFFFFF")))
+            gradientDrawableButtonSecondary.cornerRadius = 10f
+            gradientDrawableButtonSecondary.setStroke(2, Color.parseColor(returnDefaultOrValidHexColor(popupSecondaryButton.borderColor, "#FFFFFF")))
+
+            val btnSecondary: TextView = dialogView.findViewById(R.id.btn_secondary)
+            btnSecondary.background = gradientDrawableButtonSecondary
+            btnSecondary.setTextColor(Color.parseColor(returnDefaultOrValidHexColor(popupSecondaryButton.fontColor, "#000000")))
+            btnSecondary.text = popupSecondaryButton.buttonText
+            btnSecondary.setOnClickListener {
+                dialog.dismiss()
+                if (popupSecondaryButton.urlOnClick.isNotEmpty()){
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(popupSecondaryButton.urlOnClick))
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//                if (intent.resolveActivity(context.packageManager) != null)
+                    context.startActivity(intent)
+                }
+            }
+
+            dialog.show()
         }
     }
 }
