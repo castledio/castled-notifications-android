834c2ad382ac3628af3abdcc5b81e66d391e1f9a
dheeraj-7b5b7 <dheeraj.osw+dheeraj-7b5b7@gmail.com>
2023-02-21 14:59:43 +0530

    InAppChannelConfig.kt: remove commented code #39

--- notifications/src/main/java/io/castled/inAppTriggerEvents/InAppChannelConfig.kt
+++ notifications/src/main/java/io/castled/inAppTriggerEvents/InAppChannelConfig.kt
@@ -1,40 +1,5 @@
 package io.castled.inAppTriggerEvents
 
-import android.os.Bundle
-import io.castled.inAppTriggerEvents.event.EventNotification
-
-/*class InAppChannelConfig private constructor(
-    var enable: Boolean,
-    var fetchFromCloudInterval: Int) {
-
-    companion object {
-        private lateinit var inAppChannelConfig: InAppChannelConfig
-
-
-        @JvmStatic
-        fun builder(): InAppChannelConfig = if (this::inAppChannelConfig.isInitialized) inAppChannelConfig else InAppChannelConfig()
-
-    }
-
-    fun enable(enabled: Boolean): InAppChannelConfig{
-        this.enable = enabled
-        return this
-    }
-
-    fun fetchFromCloudInterval(timeInSeconds: Int): InAppChannelConfig{
-        this.fetchFromCloudInterval = timeInSeconds
-        return this
-    }
-
-    fun build(): InAppChannelConfig = this
-
-
-}*/
-
-
-
-
-
 class InAppChannelConfig internal constructor(
     internal var enable: Boolean,
     internal var fetchFromCloudInterval: Long
@@ -45,58 +10,4 @@ class InAppChannelConfig internal constructor(
         fun builder(): InAppChannelConfigBuilder = InAppChannelConfigBuilder()
 
     }
-
-    /*data class Builder internal constructor(
-        private var enabled: Boolean = false,
-                private var fetchFromCloudInterval: Int = 0
-    ){
-
-        fun enable(enabled: Boolean): Builder{
-            this.enabled = enabled
-
-            return this
-        }
-
-        fun fetchFromCloudInterval(timeInSeconds: Int): Builder{
-            this.fetchFromCloudInterval = timeInSeconds
-
-            return this
-        }
-
-        fun build(): InAppChannelConfig = InAppChannelConfig(enabled, fetchFromCloudInterval)
-
-    }*/
 }
-
-
-//This is working
-
-/*class InAppChannelConfig private constructor(): ChannelConfig() {
-    private var enabled = false
-    private var fetchFromCloudInterval = 0
-
-    companion object {
-
-        private lateinit var inAppChannelConfig: InAppChannelConfig
-
-        @JvmStatic
-        fun builder(): InAppChannelConfig =
-            if (this::inAppChannelConfig.isInitialized) inAppChannelConfig else InAppChannelConfig()
-
-    }
-
-    fun enable(enabled: Boolean): InAppChannelConfig{
-        this.enabled = enabled
-
-        return this
-    }
-
-    fun fetchFromCloudInterval(timeInSeconds: Int): InAppChannelConfig{
-        this.fetchFromCloudInterval = timeInSeconds
-
-        return this
-    }
-
-    fun build(): InAppChannelConfig = this
-}*/
-
