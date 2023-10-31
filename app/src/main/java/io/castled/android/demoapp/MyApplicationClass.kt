c2c4f52d1ef9cdb6e2e145a6d7268b0ab6d331c8
Antony Joe Mathew <antonyjoemathew@gmail.com>
2023-10-31 11:05:06 +0530

    autodismissal fix after orientation

--- app/src/main/java/io/castled/android/demoapp/MyApplicationClass.kt
+++ app/src/main/java/io/castled/android/demoapp/MyApplicationClass.kt
@@ -19,11 +19,11 @@ class MyApplicationClass : MultiDexApplication() {
         CastledNotifications.initialize(
             this,
             CastledConfigs.Builder()
-                .appId("829c38e2e359d94372a2e0d35e1f74df")
-                .location(CastledConfigs.CastledLocation.US)
+                .appId("e8a4f68bfb6a58b40a77a0e6150eca0b")
+                .location(CastledConfigs.CastledLocation.TEST)
                 .enablePush(true)
                 .enableAppInbox(true)
-                .enableInApp(false)
+                .enableInApp(true)
                 .enableTracking(false)
                 .inAppFetchIntervalSec(60)
                 .xiaomiAppId("2882303761522058544")
@@ -33,7 +33,7 @@ class MyApplicationClass : MultiDexApplication() {
         )
 
         // User-id needs to set after login flow of your app is complete
-        CastledNotifications.setUserId(this, "frank@castled.io")
+        CastledNotifications.setUserId(this, "antony@castled.io")
 
         // Listening to push notification events
         CastledNotifications.subscribeToPushNotificationEvents(object :
