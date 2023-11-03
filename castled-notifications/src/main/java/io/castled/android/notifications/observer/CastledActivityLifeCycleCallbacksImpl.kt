f2d5a0063be89a960498527b10b4a1b604b3bd05
Franklin George <frank@castled.io>
2023-11-03 16:16:31 -0400

    Optimizing the lifecycle callbacks (#60)
    * Optimizing the lifecycle callbacks
    
    * cleanups
    
    * Callback restructuring
    
    * Callback restructuring
    
    * Callback restructuring
    
    * method name rever

--- castled-notifications/src/main/java/io/castled/android/notifications/observer/CastledActivityLifeCycleCallbacksImpl.kt
+++ castled-notifications/src/main/java/io/castled/android/notifications/observer/CastledActivityLifeCycleCallbacksImpl.kt
@@ -12,16 +12,22 @@ class CastledActivityLifeCycleCallbacksImpl(private val lifeCycleListeners: List
     private var isActivityChangingConfigurations = false
 
     override fun onActivityCreated(activity: Activity, bundle: Bundle?) {
+        if (isCastledInternalActivity(activity)) {
+            return
+        }
         lifeCycleListeners.forEach { it.onActivityCreated(activity) }
     }
 
     override fun onActivityStarted(activity: Activity) {
+        if (isCastledInternalActivity(activity)) {
+            return
+        }
+        lifeCycleListeners.forEach { it.onActivityStarted(activity, isActivityChangingConfigurations) }
         if (++activityReferences == 1 && !isActivityChangingConfigurations) {
             // App enters foreground
             CastledSharedStore.isAppInBackground = false
             lifeCycleListeners.forEach { it.onAppMovedToForeground(activity) }
         }
-        lifeCycleListeners.forEach { it.onActivityStarted(activity) }
     }
 
     override fun onActivityResumed(activity: Activity) {}
@@ -29,7 +35,11 @@ class CastledActivityLifeCycleCallbacksImpl(private val lifeCycleListeners: List
     override fun onActivityPaused(activity: Activity) {}
 
     override fun onActivityStopped(activity: Activity) {
+        if (isCastledInternalActivity(activity)) {
+            return
+        }
         isActivityChangingConfigurations = activity.isChangingConfigurations
+        lifeCycleListeners.forEach { it.onActivityStopped(activity, isActivityChangingConfigurations) }
         if (--activityReferences == 0 && !isActivityChangingConfigurations) {
             CastledSharedStore.isAppInBackground = true
             lifeCycleListeners.forEach { it.onAppMovedToBackground(activity) }
@@ -39,6 +49,12 @@ class CastledActivityLifeCycleCallbacksImpl(private val lifeCycleListeners: List
     override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {}
 
     override fun onActivityDestroyed(activity: Activity) {
+        if (isCastledInternalActivity(activity)) {
+            return
+        }
         lifeCycleListeners.forEach { it.onActivityDestroyed(activity) }
     }
+
+    private fun isCastledInternalActivity(activity: Activity) =
+        activity.componentName.shortClassName.contains("CastledNotificationReceiverAct")
 }
