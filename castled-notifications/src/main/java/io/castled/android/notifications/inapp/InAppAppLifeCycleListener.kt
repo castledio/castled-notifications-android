51fbaf62b290069c4cedb319d164a515264bcdcf
Franklin George <frank@castled.io>
2024-05-06 17:13:23 -0400

    inapp lifecycle logs (#91)
    * inapp lifecucle logs
    
    * more logs added
    
    * inapp cleanups
    
    * fixes
    
    ---------
    
    Co-authored-by: Antony Joe Mathew <antonyjoemathew@gmail.com>

--- castled-notifications/src/main/java/io/castled/android/notifications/inapp/InAppAppLifeCycleListener.kt
+++ castled-notifications/src/main/java/io/castled/android/notifications/inapp/InAppAppLifeCycleListener.kt
@@ -2,6 +2,8 @@ package io.castled.android.notifications.inapp
 
 import android.app.Activity
 import io.castled.android.notifications.inapp.models.consts.AppEvents
+import io.castled.android.notifications.logger.CastledLogger
+import io.castled.android.notifications.logger.LogTags
 import io.castled.android.notifications.observer.CastledAppLifeCycleListener
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -10,16 +12,22 @@ import kotlinx.coroutines.launch
 class InAppAppLifeCycleListener(private val castledScope: CoroutineScope) :
     CastledAppLifeCycleListener {
 
+    private val logger = CastledLogger.getInstance(LogTags.IN_APP)
+
     override fun onActivityStarted(activity: Activity, isOrientationChange: Boolean) {
         InAppNotification.setCurrentActivity(activity)
+        val activityName = activity.componentName.shortClassName.drop(1)
         if (isOrientationChange) {
             InAppNotification.onOrientationChange(activity)
+            logger.debug("Orientation changed for :$activityName")
+
         } else {
             InAppNotification.logAppEvent(
                 activity,
                 AppEvents.APP_PAGE_VIEWED,
-                mapOf("name" to activity.componentName.shortClassName.drop(1))
+                mapOf("name" to activityName)
             )
+            logger.debug("Activity: $activityName started")
         }
     }
 
@@ -28,6 +36,7 @@ class InAppAppLifeCycleListener(private val castledScope: CoroutineScope) :
             InAppNotification.refreshCampaigns()
             InAppNotification.logAppEvent(activity, AppEvents.APP_OPENED, null)
         }
+        logger.debug("App in foreground")
     }
 
     override fun onActivityStopped(activity: Activity, isOrientationChange: Boolean) {
@@ -35,6 +44,7 @@ class InAppAppLifeCycleListener(private val castledScope: CoroutineScope) :
             InAppNotification.dismissInAppDialogsIfAny()
         }
         InAppNotification.clearCurrentActivity(activity)
+        logger.debug("Activity: ${activity.componentName.shortClassName.drop(1)} stopped")
     }
 
 }
