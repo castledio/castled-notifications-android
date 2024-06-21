0d2c93d25d8e87c4d7be53e9c279cd65ea157661
Antony Joe Mathew <antonyjoemathew@gmail.com>
2024-06-21 10:48:07 +0530

    logout retry handling

--- buildSrc/src/main/kotlin/Configurations.kt
+++ buildSrc/src/main/kotlin/Configurations.kt
@@ -4,6 +4,6 @@ object Configurations {
     const val targetSdk = 33
     const val minSdk = 24
     const val artifactGroup = "io.castled.android"
-    const val sdkVersion = "2.5.2"
+    const val sdkVersion = "2.5.3"
 
 }
